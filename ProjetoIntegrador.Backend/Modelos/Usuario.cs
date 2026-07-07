49dd26b4af6053751464900829e30e5aa1500ae6
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-07-07 16:46:53 -0300

    feat(modelos): cria modelo Usuario
    - Cria um modelo 'Usuario' com atributos necessários
    - Adiciona regra de negócio ao modelo criado

---
+++ ProjetoIntegrador.Backend/Modelos/Usuario.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ProjetoIntegrador.Backend.Modelos;
+
+public class Usuario
+{
+    public int Id { get; private set; }
+    public string Nome { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public DateTime CriadoEm { get; private set; }
+
+    protected Usuario()
+    {
+    }
+
+    public Usuario(string nome, string email)
+    {
+        InserirNome(nome);
+        InserirEmail(email);
+    }
+
+    private void InserirNome(string nome)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new Exception("Nome do usuário não pode estar vazio!");
+
+        if (nome.Length < 3)
+            throw new Exception("Nome do usuário precisa de no mínimo 3 caracteres!");
+
+        if (Regex.IsMatch(nome, @"[^\p{L}\s]", RegexOptions.IgnoreCase))
+            throw new Exception("Nome contém carcteres inválidos");
+
+        Nome = nome;
+    }
+
+    private void InserirEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new Exception("O email do usuário não pode estar vazio!");
+
+        if (!Regex.IsMatch(email, @"^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$", RegexOptions.IgnoreCase))
+            throw new Exception("O email do usuário não é válido!");
+
+        Email = email;
+    }
+}
---
+++ ProjetoIntegrador.Testes/ModeloUsuarioTests.cs
@@ -0,0 +1,50 @@
+﻿using ProjetoIntegrador.Backend.Modelos;
+
+namespace ProjetoIntegrador.Testes;
+
+public class ModeloUsuarioTests
+{
+    private const string Nome = "Gabriel Alves";
+    private const string Email = "gabriel@email.com";
+    
+    [Fact]
+    public void Deve_Criar_Usuario_Com_Dados_Validos()
+    {
+        var usuario = new Usuario(Nome, Email);
+
+        Assert.Equal(Nome, usuario.Nome);
+        Assert.Equal(Email, usuario.Email);
+    }
+
+    [Fact]
+    public void Deve_Lancar_Excecao_Quando_Nome_Esta_Vazio()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Usuario(string.Empty, Email)
+        );
+    }
+    
+    [Fact]
+    public void Deve_Lancar_Excecao_Quando_Nome_Esta_Invalido()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Usuario("G4bri1el", Email)
+        );
+    }
+    
+    [Fact]
+    public void Deve_Lancar_Excecao_Quando_Email_Esta_Vazio()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Usuario(Nome, string.Empty)
+        );
+    }
+    
+    [Fact]
+    public void Deve_Lancar_Excecao_Quando_Email_Esta_Invalido()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Usuario(Nome, "gabriel@email")
+        );
+    }
+}

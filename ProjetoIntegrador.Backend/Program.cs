26b6b65e12d2e68c21c735a7f5fcd745bc908683
NandaFerreira211 <mariafernandasantosferreira8@gmail.com>
2026-07-08 16:47:13 -0300

    feat(program):cria uma rota post para login
    - Cria uma nova rota 'login' para testar os conceitos de um post
    -Cria um dto para transferencia de dados de Login

--- ProjetoIntegrador.Backend/Program.cs
+++ ProjetoIntegrador.Backend/Program.cs
@@ -1,3 +1,5 @@
+using ProjetoIntegrador.Backend.Modelos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -14,4 +16,16 @@ app.UseHttpsRedirection();
 app.MapGet("/status", () => Results.Ok(new { status = "Servidor Online" }))
     .WithName("PegarStatusServidor");
 
+app.MapPost("/login/info", (LoginDto dados) =>
+{
+    try
+    {
+        var login = new Login(dados.Email, dados.Senha);
+        return Results.Created();
+    }
+    catch (Exception e)
+    {
+        return Results.BadRequest(new { message = e.Message });
+    }
+}).WithName("PegarInfoServidor");
 app.Run();

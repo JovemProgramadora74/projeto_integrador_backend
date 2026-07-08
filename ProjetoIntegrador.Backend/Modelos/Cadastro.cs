ae5a77bcfc3e2882132768feddb5e06591574660
Rebeca <rebecabarella13@gmail.com>
2026-07-08 13:52:55 -0300

    fix: corrige mudanças exigidas

--- ProjetoIntegrador.Backend/Modelos/Cadastro.cs
+++ ProjetoIntegrador.Backend/Modelos/Cadastro.cs
@@ -6,12 +6,13 @@ public class Cadastro
 {
     public string Nome { get; private set; } = string.Empty;
     public string Email { get; private set; }  = string.Empty;
-    public string Senha { get; private ; }
-    public string Username { get; private ; }
+    public string Senha { get; private set; }
+    public string Username { get; private set; }  = string.Empty;
     public DateTime CriadoEm { get; private set; }
 
     protected Cadastro()
     {
+        
     }
 
     public Cadastro( string nome, string email, string senha, string username )
@@ -25,7 +26,7 @@ public class Cadastro
     
     private void InserirNome(string nome)
     {
-        if (string.IsNullOrEmpty(nome))
+        if (string.IsNullOrWhiteSpace(nome))
             throw new Exception("Não pode estar vazio!");
 
         if (nome.Length < 3)
@@ -39,10 +40,10 @@ public class Cadastro
 
     private void InserirEmail(string email)
     {
-        if (string.IsNullOrEmpty(email))
+        if (string.IsNullOrWhiteSpace(email))
             throw new Exception("O email não pode estar vazio!");
 
-        if (Regex.IsMatch(email, @"^[^\s@]+@[^\s@]+\.[^\s@]+$", RegexOptions.IgnoreCase))
+        if (!Regex.IsMatch(email, @"^[^\s@]+@[^\s@]+\.[^\s@]+$", RegexOptions.IgnoreCase))
             throw new Exception("O Email não é valido!");
         
         Email = email;
@@ -50,24 +51,19 @@ public class Cadastro
 
     private void InserirSenha(string senha)
     {
-        if (string.IsNullOrEmpty(senha))
+        if (string.IsNullOrWhiteSpace(senha))
             throw new Exception("A senha não pode estar vazia!");
         
         if (senha.Length < 6)
-            throw new Exception("A senha precisa ter no mínomo 6 caracteres!");
+            throw new Exception("A senha precisa ter no mínimo 6 caracteres!");
         
         Senha = senha;
     }
     
     private void InserirUsername(string username)
     {
-        if (string.IsNullOrEmpty(username))
+        if (string.IsNullOrWhiteSpace(username))
             throw new Exception("O username não pode estar vazio!");
-        
-        if (username.Contains(" "))
-        {
-            throw new Exception("Não pode ter espaço no username!");
-        }
         
         if (username.Length < 3 || username.Length > 20)
             throw new Exception("O Username precisa ter entre 3 a 20 caracteres!");

70c6142dc91a485e1c5611adc27f7cbe38262943
Isabela <isabelamagella@gmail.com>
2026-07-08 14:02:21 -0300

    Corrige alterações solicitadas

--- ProjetoIntegrador.Backend/Modelos/ContatoEmergencia.cs
+++ ProjetoIntegrador.Backend/Modelos/ContatoEmergencia.cs
@@ -25,8 +25,8 @@ public class ContatoEmergencia
 
     private void InserirNome(string nome)
     {
-        if (string.IsNullOrWhiteSpace(nome)) throw new Exception("Nome do usuário não pode estar vazio!");
-        if (nome.Length < 3) throw new Exception("Nome do usuário precisa de no mínimo 3 caracteres!");
+        if (string.IsNullOrWhiteSpace(nome)) throw new Exception("Nome do contato de emergência não pode estar vazio!");
+        if (nome.Length < 3) throw new Exception("Nome do contato de emergência precisa de no mínimo 3 caracteres!");
         if (Regex.IsMatch(nome, @"[^\p{L}\s]", RegexOptions.IgnoreCase))
             throw new Exception("Nome deve conter apenas letras!");
 
@@ -43,6 +43,7 @@ public class ContatoEmergencia
 
     private void InserirTelefone(string telefone)
     {
+        if (string.IsNullOrWhiteSpace(telefone)) throw new Exception("Número de telefone não pode estar vazio!");
         if (telefone.Length != 11) throw new Exception("Número de telefone deve conter 11 caracteres!");
         if (Regex.IsMatch(telefone, "[^0-9]", RegexOptions.IgnoreCase))
             throw new Exception("O telefone deve conter apenas números!");
@@ -52,11 +53,10 @@ public class ContatoEmergencia
 
     private void InserirEmail(string email)
     {
-        if (string.IsNullOrWhiteSpace(email)) throw new Exception("Email não pode estar vazio!");
-        if (email.Length > 100) throw new Exception("Email pode ter no máximo 100 caracteres!");
+        if (string.IsNullOrWhiteSpace(email)) throw new Exception("E-mail não pode estar vazio!");
+        if (email.Length > 100) throw new Exception("E-mail pode ter no máximo 100 caracteres!");
         if (!Regex.IsMatch(email, @"[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase))
-            throw new Exception("Email inválido!");
-
+            throw new Exception("E-mail inválido!");
         Email = email;
     }
 }

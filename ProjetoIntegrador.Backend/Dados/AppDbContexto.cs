082888e2d8273d2c280189b377a938bafd3d52fd
Yatra <y.venzi@outlook.com>
2026-07-09 16:59:32 -0300

    racunho de feat

---
+++ ProjetoIntegrador.Backend/Dados/AppDbContexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoIntegrador.Backend.Modelos;
+
+namespace ProjetoIntegrador.Backend.Dados;
+
+public class AppDbContexto(DbContextOptions<AppDbContexto> options) : DbContext(options)
+{
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>().Property("Nome").HasColumnType("varchar").HasMaxLength(100).IsRequired();
+        modelBuilder.Entity<Usuario>().Property("Email").HasColumnType("varchar").HasMaxLength(100).IsRequired();
+        modelBuilder.Entity<Usuario>().HasIndex("Email").IsUnique();
+        modelBuilder.Entity<Usuario>().Property("Senha").HasColumnType("varchar").HasMaxLength(100).IsRequired();
+        modelBuilder.Entity<Usuario>().Property("Username").HasColumnType("varchar").HasMaxLength(30).IsRequired();
+        modelBuilder.Entity<Usuario>().HasIndex("Username").IsUnique();
+    }
+}

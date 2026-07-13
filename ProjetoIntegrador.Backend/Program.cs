262d46eb68f1be3e13ba5bc5d3646e7040ca1f97
Isabela <isabelamagella@gmail.com>
2026-07-13 13:58:32 -0300

    refactor(dados): refatora código de configuração de tabela

--- ProjetoIntegrador.Backend/Program.cs
+++ ProjetoIntegrador.Backend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddDbContext<AppDbContexto>(options =>
 {
     options.UseNpgsql("");
 });

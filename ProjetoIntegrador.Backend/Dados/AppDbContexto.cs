3fd6b5f448823d51a6e01aeece2fcec00a6ecde5
Sarah <sarahvrosario@gmail.com>
2026-07-13 14:03:25 -0300

    refactor(dados): refatora codigos de configuraçao da tabela

---
+++ ProjetoIntegrador.Backend/Dados/AppDbContexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoIntegrador.Backend.Modelos;
+
+namespace ProjetoIntegrador.Backend.Dados;
+
+public class AppDbContexto(DbContextOptions<AppDbContexto> options) : DbContext(options)
+{
+    public DbSet<Alerta> Alertas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Alerta>(entidade =>
+        {
+            entidade.Property("latitude").HasColumnType("decimal(10,8)").IsRequired();
+            entidade.Property("longitude").HasColumnType("decimal(10,8)").IsRequired();
+            entidade.Property("precisaogps").HasColumnType("decimal(10,8)").IsRequired();
+            entidade.Property("status").HasColumnType("varshar(10)").IsRequired();
+        });
+        
+    }
+}

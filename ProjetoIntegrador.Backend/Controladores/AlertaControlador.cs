1ad57f36e08555696d4bc993a0aa1512a133e695
Sarah <sarahvrosario@gmail.com>
2026-07-14 13:31:41 -0300

    rascunho:cria serviço para alerta

---
+++ ProjetoIntegrador.Backend/Controladores/AlertaControlador.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoIntegrador.Backend.Dados;
+using ProjetoIntegrador.Backend.Modelos;
+
+namespace ProjetoIntegrador.Backend.Controladores;
+
+public class AlertaControlador(AppDbContexto _contexto)
+{
+    public async void AddAsync(Alerta alerta)
+    {
+       await _contexto.Alertas.AddAsync(alerta);
+       await _contexto.SaveChangesAsync();
+       return alerta.Id;
+    }
+}
+

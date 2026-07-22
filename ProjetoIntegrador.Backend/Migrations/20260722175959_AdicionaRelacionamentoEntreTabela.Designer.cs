99a43d3794b283caec491c7a956f237a47d2f4d6
Isabela Magella <isabelamagella@gmail.com>
2026-07-22 15:01:37 -0300

    fix(migrations): aplica migração de relacionamento entre tabelas

--- ProjetoIntegrador.Backend/Migrations/20260722175959_AdicionaRelacionamentoEntreTabela.Designer.cs
+++ ProjetoIntegrador.Backend/Migrations/20260722175959_AdicionaRelacionamentoEntreTabela.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoIntegrador.Backend.Dados;
 
@@ -10,9 +11,11 @@ using ProjetoIntegrador.Backend.Dados;
 namespace ProjetoIntegrador.Backend.Migrations
 {
     [DbContext(typeof(AppDbContexto))]
-    partial class AppDbContextoModelSnapshot : ModelSnapshot
+    [Migration("20260722175959_AdicionaRelacionamentoEntreTabela")]
+    partial class AdicionaRelacionamentoEntreTabela
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjetoIntegrador.Backend/Migrations/20260722175959_AdicionaRelacionamentoEntreTabela.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoIntegrador.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaRelacionamentoEntreTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdUsuario",
+                table: "Contatos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Alertas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contatos_IdUsuario",
+                table: "Contatos",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alertas_UsuarioId",
+                table: "Alertas",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alertas_Usuarios_UsuarioId",
+                table: "Alertas",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contatos_Usuarios_IdUsuario",
+                table: "Contatos",
+                column: "IdUsuario",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alertas_Usuarios_UsuarioId",
+                table: "Alertas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contatos_Usuarios_IdUsuario",
+                table: "Contatos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contatos_IdUsuario",
+                table: "Contatos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alertas_UsuarioId",
+                table: "Alertas");
+
+            migrationBuilder.DropColumn(
+                name: "IdUsuario",
+                table: "Contatos");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Alertas");
+        }
+    }
+}

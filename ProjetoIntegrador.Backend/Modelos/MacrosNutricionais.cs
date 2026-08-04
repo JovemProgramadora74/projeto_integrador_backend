95564bd21c3a0c65525b0c60f9509043b0f66ad3
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-08-04 13:32:45 -0300

    cria modelos Receita e MacrosNutricionais
    - adiciona os novos modelos de Receita e MacrosNutricionais necessários no Frontend
    - adiciona métodos de validação nos modelos

---
+++ ProjetoIntegrador.Backend/Modelos/MacrosNutricionais.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjetoIntegrador.Backend.Modelos;
+
+public class MacrosNutricionais
+{
+    protected MacrosNutricionais()
+    {
+    }
+
+    public MacrosNutricionais(int proteina, int carboidrato, int gordura)
+    {
+        if (proteina < 0 || carboidrato < 0 || gordura < 0)
+            throw new Exception("Os valores de macros não podem ser negativos!");
+
+        ProteinaPorcentagem = proteina;
+        CarboidratosPorcentagem = carboidrato;
+        GordurasPorcentagem = gordura;
+    }
+
+    public int ProteinaPorcentagem { get; private set; }
+    public int CarboidratosPorcentagem { get; private set; }
+    public int GordurasPorcentagem { get; private set; }
+}
---
+++ ProjetoIntegrador.Backend/Modelos/Receita.cs
@@ -0,0 +1,63 @@
+﻿namespace ProjetoIntegrador.Backend.Modelos;
+
+public class Receita
+{
+    protected Receita()
+    {
+    }
+
+    public Receita(string titulo, string imagemUrl, string? tagRestricao, int tempoPreparoMinutos, string dificuldade,
+        MacrosNutricionais macros)
+    {
+        ValidarInserirTitulo(titulo);
+        ValidarInserirImagemUrl(imagemUrl);
+        TagRestricao = tagRestricao; // Pode ser nulo se não houver restrição
+        ValidarInserirTempoPreparo(tempoPreparoMinutos);
+        ValidarInserirDificuldade(dificuldade);
+        Macros = macros ?? throw new Exception("Os dados macro-nutricionais não podem ser nulos!");
+    }
+
+    public int Id { get; private set; }
+    public string Titulo { get; private set; } = string.Empty;
+    public string ImagemUrl { get; private set; } = string.Empty;
+    public string? TagRestricao { get; private set; }
+    public int TempoPreparoMinutos { get; private set; }
+    public string Dificuldade { get; private set; } = string.Empty;
+
+    // Propriedade de Navegação do Objeto de Valor
+    public MacrosNutricionais Macros { get; private set; } = null!;
+
+    private void ValidarInserirTitulo(string titulo)
+    {
+        if (string.IsNullOrWhiteSpace(titulo))
+            throw new Exception("O título da receita não pode estar vazio!");
+
+        Titulo = titulo;
+    }
+
+    private void ValidarInserirImagemUrl(string imagemUrl)
+    {
+        if (string.IsNullOrWhiteSpace(imagemUrl))
+            throw new Exception("A URL da imagem não pode estar vazia!");
+
+        ImagemUrl = imagemUrl;
+    }
+
+    private void ValidarInserirTempoPreparo(int tempo)
+    {
+        if (tempo <= 0)
+            throw new Exception("O tempo de preparo deve ser maior que zero minutos!");
+
+        TempoPreparoMinutos = tempo;
+    }
+
+    private void ValidarInserirDificuldade(string dificuldade)
+    {
+        if (string.IsNullOrWhiteSpace(dificuldade))
+            throw new Exception("A dificuldade não pode estar vazia!");
+
+        Dificuldade = dificuldade;
+    }
+}
+
+// Objeto de Valor para organizar os percentuais do card

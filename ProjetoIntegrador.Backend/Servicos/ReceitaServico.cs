7245675ec648ff528d9ee248d57ee886db2710d6
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-08-04 14:31:38 -0300

    cria serviço para Receita
    - adiciona uma classe para serviços de receita com o método para obter todas as receitas do banco de dados

---
+++ ProjetoIntegrador.Backend/Servicos/ReceitaServico.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoIntegrador.Backend.Dados;
+using ProjetoIntegrador.Backend.Modelos;
+
+namespace ProjetoIntegrador.Backend.Servicos;
+
+public class ReceitaServico(AppDbContexto contexto)
+{
+    public async Task<IEnumerable<ReceitaExibicaoDto>> ObterTodasReceitasAsync()
+    {
+        return await contexto.Receitas
+            .Select(r => new ReceitaExibicaoDto
+            {
+                Id = r.Id,
+                Titulo = r.Titulo,
+                ImagemUrl = r.ImagemUrl,
+                TagRestricao = r.TagRestricao,
+                TempoPreparoMinutos = r.TempoPreparoMinutos,
+                Dificuldade = r.Dificuldade,
+                Macros = new MacrosDto
+                {
+                    ProteinaPorcentagem = r.Macros.ProteinaPorcentagem,
+                    CarboidratosPorcentagem = r.Macros.CarboidratosPorcentagem,
+                    GordurasPorcentagem = r.Macros.GordurasPorcentagem
+                }
+            })
+            .ToListAsync();
+    }
+}

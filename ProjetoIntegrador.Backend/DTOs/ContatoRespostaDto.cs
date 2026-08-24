d051c19c254579a3e27a5595269ed470d63ec3b3
Gabriel Quadra <gabriel.quadra@edu.es.senac.br>
2026-08-24 16:18:15 -0300

    adiciona nova rota de contatos (#44)
    - adiciona rota para pegar contatos de uma pessoa pelo id
    - adiciona novo metodo em ContatoServico para pegar todos os contatos pelo id de um usuário
    - cria ContatoRespostaDto para envio de contatos pela rota

--- ProjetoIntegrador.Backend/DTOs/ContatoRespostaDto.cs
+++ ProjetoIntegrador.Backend/DTOs/ContatoRespostaDto.cs
@@ -1,9 +1,10 @@
 ﻿namespace ProjetoIntegrador.Backend.DTOs;
 
-public class ContatoDto
+public class ContatoRespostaDto
 {
+    public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
     public string Vinculo { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string Telefone { get; set; } = string.Empty;
 }
